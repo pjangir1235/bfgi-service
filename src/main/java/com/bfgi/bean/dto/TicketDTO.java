6056c93edafe74ee53839370aa03c324ea297512
pjangir1235 <pjluckyboy@gmail.com>
2021-02-22 18:35:42 +0530

    Added dtos

---
+++ src/main/java/com/bfgi/bean/dto/TicketDTO.java
@@ -0,0 +1,63 @@
+package com.bfgi.bean.dto;
+
+import java.util.Date;
+
+public class TicketDTO {
+    /**
+     *
+     */
+    private int id;
+    private int userId;
+    private String category;
+    private String description;
+    private Date date;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
